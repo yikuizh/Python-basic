92da4dfa680f0a841b584d7ee16bddc006e60943
yikuizh <55485922+yikuizh@users.noreply.github.com>
2019-09-21 16:16:35 +0200

    Update exercise1.py

--- 面向对象基础/exercise1.py
+++ 面向对象基础/exercise1.py
@@ -1,1 +1,13 @@
+# ex1
+# print object directly and compare two objects
+class Person:
+    pass
+
+xiaoming = Person()
+xiaohong = Person()
+
+print xiaoming
+print xiaohong
+print xiaohong == xiaoming # compare 2 objects
+
 
